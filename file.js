f5f5182b8a989750c7e309d0299c8d37c262b65d
xhu <15175265+huxiaoli1102@users.noreply.github.com>
2024-08-06 15:13:44 -0700

    Revisit Rock Paper Scissor Game

--- file.js
+++ file.js
@@ -12,62 +12,78 @@ function getComputerChoice() {
 
 
 
-function getHumanChoice() {
-    let myChoice = prompt("What is your choice?");
-    myChoice = myChoice.toUpperCase();
-    return myChoice
-}
+// function getHumanChoice() {
+//     let myChoice = prompt("What is your choice?");
+//     // myChoice = myChoice.toUpperCase();
+//     return myChoice
+// }
 
-
+let humanScore = 0;
+let computerScore = 0;
 
 
 function playRound(humanChoice, computerChoice) {
-
+    const div = document.createElement("div");
+    container.appendChild(div)
     if (humanChoice === computerChoice ) {
-        console.log("You are even!");
+        div.textContent = "You are even!";
     } else if (humanChoice === "ROCK" && computerChoice == "PAPER") {
-        console.log("You lose. Paper beats Rock!")
-        return "computer"
+        div.textContent = "You lose. Paper beats Rock!";
+        computerScore += 1;
     } else if (humanChoice === "ROCK" && computerChoice == "SCISSOR") {
-        console.log("You win. Rock beats Scissor!")
-        return "human"
+        div.textContent = "You win. Rock beats Scissor!";
+        humanScore += 1;
     } else if (humanChoice === "SCISSOR" && computerChoice == "PAPER") {
-        console.log("You win. Scissor beats Paper!")
-        return "human"
+        div.textContent = "You win. Scissor beats Paper!";
+        humanScore += 1;
     } else if (humanChoice === "SCISSOR" && computerChoice == "ROCK") {
-        console.log("You lose. Rock beats Scissor!")
-        return "computer"
+        div.textContent = "You lose. Rock beats Scissor!";
+        computerScore += 1;
     } else if (humanChoice === "PAPER" && computerChoice == "SCISSOR") {
-        console.log("You lose. Scissor beats Paper!")
-        return "computer"
+        div.textContent = "You lose. Scissor beats Paper!";
+        computerScore += 1;
     } else if (humanChoice === "PAPER" && computerChoice == "ROCK") {
-        console.log("You win. Paper beats Rock!")
-        return "human"
+        div.textContent = "You win. Paper beats Rock!";
+        humanScore += 1;
     }
+
+    if (humanScore == 5) {
+        alert("Game is over! You win!");
+    } else if (computerScore == 5) {
+        alert("Game is over! You lose!");
+    }
+        
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 3; i++){
-        hChoice = getHumanChoice();
-        cChoice = getComputerChoice();
-        console.log(hChoice);
-        console.log(cChoice);
-        let winner = playRound(hChoice, cChoice);
-        if (winner === 'computer') {
-            computerScore += 1;
-        } else if (winner === 'human'){
-            humanScore +=1;
-        } 
-            
-        }
-    
-    return [humanScore, computerScore]
+// function playGame() {
+//     let humanScore = 0;
+//     let computerScore = 0;
 
-    }
+//     if (result === 'computer') {
+//         computerScore += 1;
+//     } else if (result === 'human'){
+//         humanScore +=1;
+//     } 
+        
+//     }
     
+    // return [humanScore, computerScore]
 
-const [humanFinalScore, computerFinalScore] = playGame();
-console.log(humanFinalScore);
-console.log(computerFinalScore);
+
+const container = document.querySelector("body");
+const btnRock = document.createElement("button");
+btnRock.textContent = "ROCK";
+const btnPaper = document.createElement("button");
+btnPaper.textContent = "PAPER";
+const btnScissor = document.createElement("button");
+btnScissor.textContent = "SCISSOR";
+
+container.appendChild(btnRock);
+container.appendChild(btnPaper);
+container.appendChild(btnScissor);
+
+
+btnRock.addEventListener("click", () => {playRound("ROCK", getComputerChoice())});
+btnPaper.addEventListener("click", () => {playRound("PAPER", getComputerChoice())});
+btnScissor.addEventListener("click", () => {playRound("SCISSOR", getComputerChoice())});
+
